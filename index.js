7543c2de449de79641cd8d77df51014cc3562475
atorresruiz8 <atorresruiz8@gmail.com>
2023-03-16 13:54:37 -0700

    added eventListeners to each button

--- index.js
+++ index.js
@@ -3,6 +3,9 @@
 // Otherwise, use default array
 let uncapProgress;
 
+// An array to store the button objects
+let arcarum = [];
+
 // Retrieve localStorage
 const savedProgress = JSON.parse(localStorage.getItem("uncapProgress"));
 
@@ -11,7 +14,7 @@ if (Array.isArray(savedProgress)) {
   uncapProgress = savedProgress;
 } else {
   uncapProgress = [];
-  console.log(uncapProgress);
+  //console.log(uncapProgress);
 }
 
 
@@ -28,11 +31,46 @@ const moonButton = document.getElementById("moon-button");
 const sunButton = document.getElementById("sun-button");
 const judgementButton = document.getElementById("judgement-button");
 
+// Store all of the buttons in the array for easier access
+arcarum.push(
+  justiceButton, hangedManButton, deathButton,
+  temperanceButton, devilButton, towerButton,
+  starButton, moonButton, sunButton, judgementButton
+  );
+
 const render = () => {
-  
+  // Add an event listener to each button to know which has
+  // been clicked recently
+  arcarum.forEach((button) => {
+    button.addEventListener("click", handleClick);
+  });
 }
 
 // Controller
-const getButtonClicked = () => {
-
+const handleClick = (event) => {
+  const buttonText = event.target.textContent;
+  if (buttonText === "Rise of Justice") {
+    console.log("justice button was pressed");
+  } else if (buttonText === "Binds of the Hanged Man") {
+    console.log("hanged man button was pressed");
+  } else if (buttonText === "Pain of Death") {
+    console.log("death button was pressed");
+  } else if (buttonText === "Theater of Temperance") {
+    console.log("temperance button was pressed");
+  } else if (buttonText === "Kiss of the Devil") {
+    console.log("devil button was pressed");
+  } else if (buttonText === "Collapse of the Tower") {
+    console.log("tower button was pressed");
+  } else if (buttonText === "Shooting of the Star") {
+    console.log("star button was pressed");
+  } else if (buttonText === "Reflection of the Moon") {
+    console.log("moon button was pressed");
+  } else if (buttonText === "Heat of the Sun") {
+    console.log("sun button was pressed");
+  } else if (buttonText === "Melody of Judgement") {
+    console.log("judgement button was pressed");
+  }
+  
 }
+
+render();
